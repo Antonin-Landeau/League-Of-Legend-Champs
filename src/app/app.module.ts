1a425ea7578089e38a2679e4c71c02c12c227732
Antonin-Landeau <alandeau@charles-poncet.net>
2023-05-25 12:12:48 +0200

    animation to cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -39,7 +40,12 @@ import { HomePageTitleComponent } from './components/home-page/home-page-title/h
     ButtonComponent,
     HomePageTitleComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/champions-page/champion-card/champion-card.component.ts
+++ src/app/components/champions-page/champion-card/champion-card.component.ts
@@ -1,10 +1,28 @@
 import { Component, Input } from '@angular/core';
 import { Champion } from 'src/app/types/champions';
 
+import { trigger, transition, style, animate } from '@angular/animations';
+
+const enterTransition = transition(':enter', [
+  style({
+    opacity: 0,
+  }),
+  animate('1s ease-in', style({ opacity: 1 })),
+]);
+const exitTransition = transition(':leave', [
+  style({
+    opacity: 1,
+  }),
+  animate('1s ease-in', style({ opacity: 0 })),
+]);
+const fadeIn = trigger('fadeIn', [enterTransition]);
+const fadeOut = trigger('fadeOut', [exitTransition]);
+
 @Component({
   selector: 'app-champion-card',
   templateUrl: './champion-card.component.html',
   styleUrls: ['./champion-card.component.css'],
+  animations: [fadeIn, fadeOut],
 })
 export class ChampionCardComponent {
   @Input() champion?: Champion = undefined;
